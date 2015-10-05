7db008c958ae348689276ff0b0771e1500d1f4fa
zhangmeng19 <zhangmeng19@meituan.com>
2015-10-05 11:37:54 +0800

    test2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,7 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] argv) {
-		System.out.println("hello world");
+		System.out.println("hello world!");
+		System.out.println("hello world test!");
 	}
 }
