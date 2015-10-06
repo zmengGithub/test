df68feaf431b361429daff0d8892b185c6132668
zmengGithub <zmeng_pr@163.com>
2015-10-06 13:33:44 +0800

    check push

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] argv) {
 		System.out.println("hello world!");
 		System.out.println("hello world test!");
+
+		System.out.println("hello world check push!");
 	}
 }
